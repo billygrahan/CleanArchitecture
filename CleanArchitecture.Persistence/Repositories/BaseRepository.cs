debfa4d9b585607a058e2399a45d040066166480
billygrahan <billygrahan@alu.ufc.br>
2024-05-28 17:04:16 -0300

    implementado as classes, conexão com o banco de dados e a logica das requisições (get,post,put,delete)

---
+++ CleanArchitecture.Persistence/Repositories/BaseRepository.cs
@@ -0,0 +1,48 @@
+﻿using CleanArchitecture.Domain.Entities;
+using CleanArchitecture.Domain.Interfaces;
+using CleanArchitecture.Persistence.Context;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Persistence.Repositories
+{
+    public class BaseRepository<T> : IBaseRepository<T> where T : BaseEntities
+    {
+        protected readonly AppDbContext Context;
+        public BaseRepository(AppDbContext context) 
+        {
+            Context = context;
+        }
+        public void Create(T entity)
+        {
+            entity.DateCreated = DateTime.UtcNow;
+            Context.Add(entity);
+        }
+
+        public void Delete(T entity)
+        {
+            entity.DateDeleted = DateTime.UtcNow;
+            Context.Remove(entity);
+        }
+
+        public async Task<T> Get(Guid id, CancellationToken cancellationToken)
+        {
+            return await Context.Set<T>().FirstOrDefaultAsync(x => x.Id == id, cancellationToken);
+        }
+
+        public async Task<List<T>> GetAll(CancellationToken cancellationToken)
+        {
+            return await Context.Set<T>().ToListAsync(cancellationToken);
+        }
+
+        public void Update(T entity)
+        {
+            entity.DateUpdate = DateTime.UtcNow;
+            Context.Update(entity);
+        }
+    }
+}
---
+++ Domain/Entities/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Domain.Entities
+{
+    public sealed class User : BaseEntities
+    {
+        public string? Email { get; set; }
+        public string? Name { get; set; }
+    }
+}
